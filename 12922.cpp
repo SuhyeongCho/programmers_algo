f4276a1fe1afbc930f4a9b37618f56f3456543b2
SuhyeongCho <ch5951130@naver.com>
2020-12-29 14:55:05 +0900

    수박수박수박수박수박수?

---
+++ 12922.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i = 0 ; i < n ; i++) {
+        if (i % 2) answer += "박";
+        else answer += "수";
+    }
+    return answer;
+}

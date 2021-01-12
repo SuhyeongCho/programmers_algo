8c1dc15a7e79bc65535d586f646d175b57fbcc65
SuhyeongCho <ch5951130@naver.com>
2021-01-12 16:01:27 +0900

    구명보트

---
+++ 42885.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int left = 0, right = people.size() - 1;
+    
+    sort(people.begin(), people.end());
+    
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+            right--;
+        } else {
+            right--;
+        }
+        answer++;
+    }
+    
+    return answer;
+}
